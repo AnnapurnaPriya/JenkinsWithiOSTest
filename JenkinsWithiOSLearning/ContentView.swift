9df457491ee216c5a6288b36eb6389442d8336d4
Annapurna Priya <annapurna.priya@digitas.com>
2022-02-12 13:18:58 +0530

    Initial Commit

---
+++ JenkinsWithiOSLearning/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  JenkinsWithiOSLearning
+//
+//  Created by Annapurna Priya on 12/02/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ JenkinsWithiOSLearning/JenkinsWithiOSLearningApp.swift
@@ -0,0 +1,17 @@
+//
+//  JenkinsWithiOSLearningApp.swift
+//  JenkinsWithiOSLearning
+//
+//  Created by Annapurna Priya on 12/02/22.
+//
+
+import SwiftUI
+
+@main
+struct JenkinsWithiOSLearningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
